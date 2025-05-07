39396a09fee239fb8b4d334cb1892e14136da420
Daniel Tri <45452945+danielrudianto@users.noreply.github.com>
2025-05-07 23:53:06 +0700

    Working on login functionality

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  constructor(private apiService: ApiService, private snackBar: MatSnackBar) {}
 
+  isSubmitting: boolean = false;
+
+  loginFormGroup: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+  });
+
+  onSubmit() {
+    this.apiService.post('auth', this.loginFormGroup.value).subscribe({
+      next: (data) => {
+        console.log(data);
+      },
+      error: (error) => {
+        this.snackBar.open(error.error.detail, 'Close', {
+          duration: 3000,
+        });
+      },
+    });
+  }
 }
